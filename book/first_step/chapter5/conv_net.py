25dd876eb9fde06c2810c393995d9d115b98b0b6
unknown <profile2697@gmail.com>
2016-12-28 13:31:31 +0900

    [Refactoring] 그래프 정의 부분을 분리

--- book/first_step/chapter5/conv_net.py
+++ book/first_step/chapter5/conv_net.py
@@ -29,36 +29,7 @@ def main():
 
     x_image = tf.reshape(x, [-1, 28, 28, 1])
 
-    # First Layer
-    W_conv1 = weight_variable([5, 5, 1, 32])
-    b_conv1 = bias_variable([32])
-
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
-
-    # Second Layer
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
-
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    h_pool2 = max_pool_2x2(h_conv2)
-
-    # Densely Connected Layer
-    W_fc1 = weight_variable([7 * 7 * 64, 1024])  # 7 * 7짜리 map 이 64개 있고, 이것을 1024개의 뉴런에 연결할 것이므로
-    b_fc1 = bias_variable([1024])
-
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])  # h_pool2를 flat 한 vector 로 변형
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-    # For Dropout
-    keep_prob = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-    # SoftMax Layer
-    W_fc2 = weight_variable([1024, 10])
-    b_fc2 = bias_variable([10])
-
-    y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+    keep_prob, y_conv = construct_conv_net(x_image)
 
     # For Learning
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
@@ -78,6 +49,37 @@ def main():
 
     print("test accuracy %g" % sess.run(accuracy, feed_dict={x: mnist.test.images,
                                                              y_: mnist.test.labels, keep_prob: 1.0}))
+
+
+def construct_conv_net(x_image):
+    # First Layer
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
+
+    # Second Layer
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
+
+    # Densely Connected Layer
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])  # 7 * 7짜리 map 이 64개 있고, 이것을 1024개의 뉴런에 연결할 것이므로
+    b_fc1 = bias_variable([1024])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])  # h_pool2를 flat 한 vector 로 변형
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+    # For Dropout
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    # SoftMax Layer
+    W_fc2 = weight_variable([1024, 10])
+    b_fc2 = bias_variable([10])
+    y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+    return keep_prob, y_conv
 
 
 if __name__ == "__main__":
