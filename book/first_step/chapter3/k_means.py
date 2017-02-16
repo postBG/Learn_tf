7159543f76691d4f05d311e7e10b62a624ef513e
unknown <profile2697@gmail.com>
2017-02-16 13:25:18 +0900

    [chapter3, refactoring] extract some local variables

--- book/first_step/chapter3/k_means.py
+++ book/first_step/chapter3/k_means.py
@@ -36,20 +36,21 @@ def run_training(vectors_set, k=4, epoch=100):
     expanded_vectors = tf.expand_dims(vectors, 0)
     expanded_centroids = tf.expand_dims(centroids, 1)
 
-    assignments = tf.argmin(tf.reduce_sum(tf.square(tf.sub(expanded_vectors, expanded_centroids)), 2), 0)
+    distances = tf.reduce_sum(tf.square(tf.sub(expanded_vectors, expanded_centroids)), 2)  # shape => k * num_points
+    assignments = tf.argmin(distances, 0)  # shape => num_points
 
     means = tf.concat(0, [tf.reduce_mean(tf.gather(vectors,
                                                    tf.reshape(tf.where(tf.equal(assignments, c)), [1, -1])),
                                          reduction_indices=[1]) for c in range(k)])
 
-    updated_centroids = tf.assign(centroids, means)
+    update_centroids = tf.assign(centroids, means)
 
     init_op = tf.global_variables_initializer()
 
     with tf.Session() as sess:
         sess.run(init_op)
         for step in range(epoch):
-            _, centroid_values, assignment_values = sess.run([updated_centroids, centroids, assignments])
+            _, centroid_values, assignment_values = sess.run([update_centroids, centroids, assignments])
 
         return centroid_values, assignment_values
 
@@ -65,6 +66,7 @@ def plot_results(vectors_set, assignments):
     df = pd.DataFrame(data)
     sns.lmplot("x", "y", data=df, fit_reg=False, size=6, hue="cluster", legend=False)
     plt.show()
+
 
 if __name__ == "__main__":
     vectors_set = generate_random_points()
